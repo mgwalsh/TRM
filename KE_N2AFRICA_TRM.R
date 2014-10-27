8073e3c2e53da1e183e658fb73ddf0518b7bf01b
mgwalsh <markusgwalsh@gmail.com>
2014-10-27 17:10:27 +0300

    added dismo profile models

--- KE_N2AFRICA_TRM.R
+++ KE_N2AFRICA_TRM.R
@@ -16,73 +16,76 @@ require(dismo)
 # Data downloads ----------------------------------------------------------
 
 download("https://www.dropbox.com/s/u8o9935k3r4hdyx/VISI_trials.csv?dl=0", "VISI_trials.csv", mode="wb")
-geot <- read.table("VISI_trials.csv", header=T, sep=",")
+geos <- read.table("VISI_trials.csv", header=T, sep=",")
 
 # Kenya Gtif download (~22 Mb)
 download("https://www.dropbox.com/s/wg7k2ff1snge8h9/KE_grids.zip?dl=0", "KE_grids.zip", mode="wb")
 unzip("KE_grids.zip", overwrite=T)
 
-# generate LAEA CRS & grid ID's -------------------------------------------
+# Generate LAEA CRS & grid ID's -------------------------------------------
 
 # Project to Africa LAEA from LonLat
-geot.laea <- as.data.frame(project(cbind(geot$Lon, geot$Lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
-colnames(geot.laea) <- c("x","y")
-geot <- cbind(geot, geot.laea)
+geos.laea <- as.data.frame(project(cbind(geos$Lon, geos$Lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
+colnames(geos.laea) <- c("x","y")
+geos <- cbind(geos, geos.laea)
 
 # Generate AfSIS grid cell ID's (GID)
 res.pixel <- 1000
-xgid <- ceiling(abs(geot$x)/res.pixel)
-ygid <- ceiling(abs(geot$y)/res.pixel)
-gidx <- ifelse(geot$x<0, paste("W", xgid, sep=""), paste("E", xgid, sep=""))
-gidy <- ifelse(geot$y<0, paste("S", ygid, sep=""), paste("N", ygid, sep=""))
+xgid <- ceiling(abs(geos$x)/res.pixel)
+ygid <- ceiling(abs(geos$y)/res.pixel)
+gidx <- ifelse(geos$x<0, paste("W", xgid, sep=""), paste("E", xgid, sep=""))
+gidy <- ifelse(geos$y<0, paste("S", ygid, sep=""), paste("N", ygid, sep=""))
 GID <- paste(gidx, gidy, sep="-")
-geot.gid <- cbind(geot, GID)
-coordinates(geot.gid) = ~x+y
-proj4string(geot.gid) = CRS("+proj=laea +datum=WGS84 +ellps=WGS84 +lat_0=5 +lon_0=20 +no_defs")
+geos.gid <- cbind(geos, GID)
 
-# Grid overlay ------------------------------------------------------------
+# Distribution model (DM) setup -------------------------------------------
 
-grid.list <- c("BSANs.tif","BSASs.tif","BSAVs.tif","CTIs.tif","ELEVs.tif","EVIAs.tif","LSTDs.tif","LSTNs.tif","REF1s.tif","REF2s.tif","REF3s.tif","REF7s.tif","RELIs.tif","TMAPs.tif","TMFIs.tif")
-for (i in 1:length(grid.list)){
-  print(paste("extracting", grid.list[i]))
-  grids <- raster(grid.list[i])
-  geot.gid@data[strsplit(grid.list[i], split=".tif")[[1]]] <- extract(
-    x = grids, 
-    y = geot.gid,
-    method = "simple")
-}
-tgrid <- as.data.frame(geot.gid)
-write.csv(tgrid, "VISI_dat.csv")
-
-# Environmental (dis)similarities to trial locations (LID'S) --------------
-
-LID <- aggregate(tgrid[,1:2], by=list(LID=tgrid$LID), mean)
-glist <- list.files(pattern='tif', full.names=TRUE)
+pres <- aggregate(geos.gid[,9:10], by=list(GID=geos.gid$GID), mean)
+glist <- list.files(pattern='tif', full.names=T)
 grids <- stack(glist)
-mahal <- mahal(grids, LID[,2:3])
-preds <- predict(grids, mahal)
-plot(preds)
 
-# Export Gtif
-writeRaster(preds, filename=“VISI_mahal”, format= "GTiff", overwrite=T)
-
-# Trial distribution model setup ------------------------------------------
-
-# Generate a "x" km (specify) Region of Interest (ROI) buffer around existing LID's
-x <- 30000
-coordinates(LID) <- ~x+y
-proj4string(LID) <- CRS("+proj=laea +datum=WGS84 +ellps=WGS84 +lat_0=5 +lon_0=20 +units=m +no_defs")
-buffer <- circles(LID, d=x, lonlat=F)
+# Generate a "x" (specify) km Region of Interest (ROI) buffer around existing GID's
+x <- 25000
+coordinates(pres) <- ~x+y
+proj4string(pres) <- CRS("+proj=laea +datum=WGS84 +ellps=WGS84 +lat_0=5 +lon_0=20 +units=m +no_defs")
+buffer <- circles(pres, d=x, lonlat=F)
 roi <- gUnaryUnion(buffer@polygons)
 
-# Randomly sample the ROI background with n (specify) * no. of trial LID's
-n <- 5
+# Randomly sample the ROI background with B (specify) * no. of trial GID's samples
+B <- 30
 ext <- extent(roi)
-bsamp <- randomPoints(grids, n=n*length(LID), ext=ext, extf = 1.25)
-backg <- extract(grids, bsamp)
+set.seed(1235813)
+back <- randomPoints(grids, n=B*length(pres), ext=ext, extf = 1)
+gback <- extract(grids, back)
 
-# Plot ROI, trial (LID) and background sample locations
-plot(roi, axes=T)
-points(LID, pch=3, col="blue")
-points(bsamp, pch=3, col="red", cex=0.5)
+# Plot ROI, trial GID's and background sample locations
+plot(ext, xlab="Easting (m)", ylab="Northing (m)")
+plot(roi, add=T)
+points(back, pch=3, col="grey", cex=0.5)
+points(pres, pch=21, col="red", bg="red")
+
+# Dataframe
+gpres <- extract(grids, pres)
+pb <- c(rep(1, nrow(pres)), rep(0, nrow(back)))
+presback <- data.frame(cbind(pb, rbind(gpres, gback)))
+
+# dismo profile models ----------------------------------------------------
+
+# Bioclim (for comparison to Mahalanobis distance only)
+bc <- bioclim(grids, pres)
+ebc <- evaluate(pres, back, bc, grids)
+plot(ebc, "ROC")
+
+# Mahalanobis distance
+mh <- mahal(grids, pres)
+emh <- evaluate(pres, back, mh, grids)
+plot(emh, "ROC")
+mhp <- predict(grids, mh, ext=ext)
+mhp[mhp < -1000] <- -1000
+plot(mhp)
+mht <- threshold(emh, "spec_sens")
+plot(mhp>mht)
+
+
+
 
