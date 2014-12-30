21847e34af997f4a9617cc737a8a0bf914747fea
mgwalsh <markusgwalsh@gmail.com>
2014-12-30 06:04:08 +0300

    Documentation update

--- MW_LREP_ensemble.R
+++ MW_LREP_ensemble.R
@@ -183,12 +183,12 @@ plot(siens.pred, axes = F)
 
 # Write spatial predictions -----------------------------------------------
 # Create a "LREP_Results" folder in current working directory
-dir.create("LREP_results", showWarnings=F)
+dir.create("LREP_Results", showWarnings=F)
 
 # Export Gtif's to "./LREP_results"
-writeRaster(yc.preds, filename="./LREP_results/LREP_ycpreds.tif", datatype="FLT4S", options="INTERLEAVE=BAND", overwrite=T)
-writeRaster(si.preds, filename="./LREP_results/LREP_sipreds.tif", datatype="FLT4S", options="INTERLEAVE=BAND", overwrite=T)
+writeRaster(yc.preds, filename="./LREP_Results/LREP_ycpreds.tif", datatype="FLT4S", options="INTERLEAVE=BAND", overwrite=T)
+writeRaster(si.preds, filename="./LREP_Results/LREP_sipreds.tif", datatype="FLT4S", options="INTERLEAVE=BAND", overwrite=T)
 # Ensemble predictions
 enspred <- stack(ycens.pred, siens.pred)
-writeRaster(enspred, filename="./LREP_results/LREP_enspred.tif", datatype="FLT4S", options="INTERLEAVE=BAND", overwrite=T)
+writeRaster(enspred, filename="./LREP_Results/LREP_enspred.tif", datatype="FLT4S", options="INTERLEAVE=BAND", overwrite=T)
 
