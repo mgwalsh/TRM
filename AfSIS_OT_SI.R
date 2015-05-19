38f992ed4579b530e42370a92af0b08893785ddd
mgwalsh <markusgwalsh@gmail.com>
2015-05-19 14:47:19 +0300

    Documentation update

--- AfSIS_OT_SI.R
+++ AfSIS_OT_SI.R
@@ -1,4 +1,4 @@
-#+ Small sample site index models from AfSIS omission trial data
+#+ Example site index models from AfSIS omission trial data
 #+ AfSIS ommission trial sample data, courtesy of CIAT
 #+ M. Walsh, May 2015
 
@@ -8,7 +8,7 @@ require(downloader)
 require(caret)
 require(fastICA)
 
-#+ Data download ----------------------------------------------------------
+#+ Data download ---------------------------------------------------------
 # Create a "Data" folder in your current working directory
 dir.create("OT_data", showWarnings=F)
 dat_dir <- "./OT_data"
@@ -20,7 +20,7 @@ field <- read.table(paste(dat_dir, "/Fields.csv", sep=""), header=T, sep=",")
 spect <- read.table(paste(dat_dir, "/Spectra.csv", sep=""), header=T, sep=",")
 
 #+ Spectral data transforms <caret> --------------------------------------
-# Spectral principal components analysis (PCA) transform
+# Principal components analysis (PCA) transform
 pcatr <- preProcess(spect[,3:1762], method=c("BoxCox", "center", "scale", "pca"), pcaComp=3)
 sppca <- predict(pcatr, spect[,3:1762])
 
@@ -28,7 +28,7 @@ sppca <- predict(pcatr, spect[,3:1762])
 icatr <- preProcess(spect[,3:1762], method=c("BoxCox", "center", "scale", "ica"), n.comp=3)
 spica <- predict(icatr, spect[,3:1762])
 
-#+ Merge spectral transforms with field measurements  ---------------------
+#+ Merge spectral transforms with field measurements ---------------------
 speci <- cbind(spect[,1:2], sppca, spica)
 avesp <- aggregate(speci[,3:8], by=list(speci$FieldID), FUN="mean")
 colnames(avesp) <- c("FieldID","PCA1","PCA2","PCA3","ICA1","ICA2","ICA3")
@@ -36,9 +36,3 @@ afotd <- merge(field, avesp, by="FieldID")
 
 #+ Write data file --------------------------------------------------------
 write.csv(afotd, "./OT_data/OT_data.csv", row.names=F)
-
-
-
-
-
-
