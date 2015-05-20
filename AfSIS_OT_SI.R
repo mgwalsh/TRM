cd385a1c0d63a303b2f15925eddaf55361f137ed
mgwalsh <markusgwalsh@gmail.com>
2015-05-20 05:34:43 +0300

    tuning grid adjustment

--- AfSIS_OT_SI.R
+++ AfSIS_OT_SI.R
@@ -3,10 +3,11 @@
 #+ M. Walsh, May 2015
 
 #+ Required packages
-# install.packages(c("downloader","caret","fastICA")), dependencies=TRUE)
+# install.packages(c("downloader","caret","fastICA","glmnet")), dependencies=TRUE)
 require(downloader)
 require(caret)
 require(fastICA)
+require(glmnet)
 
 #+ Data download ---------------------------------------------------------
 # Create a "Data" folder in your current working directory
@@ -24,15 +25,41 @@ spect <- read.table(paste(dat_dir, "/Spectra.csv", sep=""), header=T, sep=",")
 pcatr <- preProcess(spect[,3:1762], method=c("BoxCox", "center", "scale", "pca"), pcaComp=3)
 sppca <- predict(pcatr, spect[,3:1762])
 
-# Independent components analysis (ICA) transform
-icatr <- preProcess(spect[,3:1762], method=c("BoxCox", "center", "scale", "ica"), n.comp=3)
-spica <- predict(icatr, spect[,3:1762])
+# not run: Independent components analysis (ICA) transform
+# icatr <- preProcess(spect[,3:1762], method=c("BoxCox", "center", "scale", "ica"), n.comp=3)
+# spica <- predict(icatr, spect[,3:1762])
 
 #+ Merge spectral transforms with field measurements ---------------------
-speci <- cbind(spect[,1:2], sppca, spica)
-avesp <- aggregate(speci[,3:8], by=list(speci$FieldID), FUN="mean")
-colnames(avesp) <- c("FieldID","PCA1","PCA2","PCA3","ICA1","ICA2","ICA3")
+speci <- cbind(spect[,1:2], sppca)
+avesp <- aggregate(speci[,3:5], by=list(speci$FieldID), FUN="mean")
+colnames(avesp) <- c("FieldID","PCA1","PCA2","PCA3")
 afotd <- merge(field, avesp, by="FieldID")
 
 #+ Write data file --------------------------------------------------------
 write.csv(afotd, "./OT_data/OT_data.csv", row.names=F)
+
+#+ Regularized regression models ------------------------------------------
+set.seed(1235813)
+
+tc <- trainControl(
+  method = "repeatedCV",
+  number = 10,
+  repeats = 5,
+  returnResamp = "all"
+ )
+
+# Control yields (Yc)
+Yc.spca <- train(log(Yc) ~ PCA1 + PCA2 + PCA3, data = afotd,
+                family = "gaussian", 
+                method = "glmnet",
+                tuneGrid = expand.grid(.alpha=seq(0.1,0.5, by=0.1),.lambda=seq(0,0.3,by=0.01)),
+                trControl = tc)
+
+Yc.wetc <- train(log(Yc) ~ pH + Sand + N + P + K, data = afotd,
+                standardize = TRUE,
+                family = "gaussian", 
+                method = "glmnet",
+                tuneGrid = expand.grid(.alpha=seq(0.1,0.5, by=0.1),.lambda=seq(0,0.3,by=0.01)),
+                trControl = tc)
+
+
