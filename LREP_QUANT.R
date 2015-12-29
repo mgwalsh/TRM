65b11633d7eb7a38fdf1bbc4da5f8490bda010e3
mgwalsh <markusgwalsh@gmail.com>
2015-12-29 17:33:07 +0300

    Qunatile regression of maize yield responses

---
+++ LREP_QUANT.R
@@ -0,0 +1,45 @@
+#' Quantile regressions of Maize yield responses to fertilizer applications
+#' Malawi LREP response trial data (courtesy of LREP)
+#' LREP data documentation at: https://www.dropbox.com/s/4qbxnz4mdl92pdv/Malawi%20area-specific%20fertilizer%20recs%20report.pdf?dl=0
+#' M. Walsh, December 2015
+
+# Required packages
+# install.packages(c("downloader","quantreg")), dependencies=TRUE)
+require(downloader)
+require(quantreg)
+
+# Data setup --------------------------------------------------------------
+# Create a "Data" folder in your current working directory
+dir.create("MW_data", showWarnings=F)
+setwd("./MW_data")
+
+# download LREP fertilizer response data
+download("https://www.dropbox.com/s/i4dby04fl9j042a/MW_fert_trials.zip?dl=0", "MW_fert_trials.zip", mode="wb")
+unzip("MW_fert_trials.zip", overwrite=T)
+sites <- read.table("Location.csv", header=T, sep=",")
+trial <- read.table("Trial.csv", header=T, sep=",")
+mresp <- merge(sites, trial, by="LID")
+mresp <- mresp[order(mresp$Yt),] ## order dataframe based on treated yield (Yt)
+mresp$Year <- mresp$Year-1996
+
+# Exploratory plots -------------------------------------------------------
+trt1 <- subset(mresp, NPS==1 & Urea==1, select=c(Yt,Yc)) 
+trt2 <- subset(mresp, NPS==2 & Urea==2, select=c(Yt,Yc)) 
+trt3 <- subset(mresp, NPS==2 & Urea==3, select=c(Yt,Yc))
+plot(ecdf(mresp$Yc), main="", xlab="Maize yield (kg/ha)", ylab="Cum. proportion of observations", xlim=c(-50, 8050), verticals=T, lty=1, lwd=2, col="red", do.points=F)
+abline(0.5,0, lty=2, col="grey")
+plot(ecdf(trt1$Yt), add=T, verticals=T, lty=1, lwd=1, col="grey", do.points=F)
+plot(ecdf(trt2$Yt), add=T, verticals=T, lty=1, lwd=1, col="grey", do.points=F)
+plot(ecdf(trt3$Yt), add=T, verticals=T, lty=1, lwd=1, col="grey", do.points=F)
+
+# Quantile regression -----------------------------------------------------
+attach(mresp)
+Y <- cbind(Yt)
+X <- cbind(Yc,Year,NPS,Urea)
+Resp.rq <- rq(Y~X, tau = seq(0.05, 0.95, by = 0.05), data = mresp)
+detach(mresp)
+
+# Results plots
+summary <- summary(Resp.rq)
+plot(summary)
+
