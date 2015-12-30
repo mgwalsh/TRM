22d571f59b01580d85ef119af48ba81931f532b6
mgwalsh <markusgwalsh@gmail.com>
2015-12-30 12:10:35 +0300

    Quatile regression of MW maize yields

--- LREP_QUANT.R
+++ LREP_QUANT.R
@@ -31,7 +31,7 @@ abline(c(0,1), col = "red", lwd = 2)
 abline(rq(Yt~Yc, tau=0.5, data=mresp), col="blue", lwd = 2)
 tau <- c(.025,.25,.75,.975)
 for(i in 1:length(tau)) {
-    abline(rq(Yt~Yc, tau=tau[i], data = mresp), col = "black", lty = 2, lwd = 1) }
+    abline(rq(Yt~Yc, tau=tau[i], data = mresp), col = "blue", lty = 2, lwd = 1) }
 
 # ECDF plot
 trt1 <- subset(mresp, NPS==1 & Urea==1, select=c(Yt,Yc)) 
@@ -39,9 +39,9 @@ trt2 <- subset(mresp, NPS==2 & Urea==2, select=c(Yt,Yc))
 trt3 <- subset(mresp, NPS==2 & Urea==3, select=c(Yt,Yc))
 plot(ecdf(mresp$Yc), main="", xlab="Maize yield (kg/ha)", ylab="Cum. proportion of observations", xlim=c(-50, 8050), verticals=T, lty=1, lwd=2, col="red", do.points=F)
 abline(0.5,0, lty=2, col="grey")
-plot(ecdf(trt1$Yt), add=T, verticals=T, lty=1, lwd=1, col="grey", do.points=F)
-plot(ecdf(trt2$Yt), add=T, verticals=T, lty=1, lwd=1, col="grey", do.points=F)
-plot(ecdf(trt3$Yt), add=T, verticals=T, lty=1, lwd=1, col="grey", do.points=F)
+plot(ecdf(trt1$Yt), add=T, verticals=T, lty=1, lwd=1, col="blue", do.points=F)
+plot(ecdf(trt2$Yt), add=T, verticals=T, lty=1, lwd=1, col="blue", do.points=F)
+plot(ecdf(trt3$Yt), add=T, verticals=T, lty=1, lwd=1, col="blue", do.points=F)
 
 # Quantile regression -----------------------------------------------------
 attach(mresp)
