a857d67874e4be3aababcbb26586e3cead8c2d60
mgwalsh <markusgwalsh@gmail.com>
2018-07-07 10:22:44 +0300

    OAF 2016/2017 Maize yield gap, Western Kenya data setup

--- OAF_yield_data 2016:17.R
+++ OAF_yield_data 2016:17.R
@@ -9,7 +9,6 @@ suppressPackageStartupMessages({
   require(rgdal)
   require(raster)
   require(quantreg)
-  require(arm)
   require(leaflet)
   require(htmlwidgets)
 })
@@ -22,7 +21,8 @@ setwd("./OAF_data")
 # download OAF yield data
 # download("", "", mode = "wb") ## insert OAF data link here
 # unzip("oafdata.csv.zip", overwrite = T)
-yield <- read.table("oafdata.csv", header = T, sep = ",")
+yield <- read.table("oafyga.csv", header = T, sep = ",")
+yield <- yield[!duplicated(yield), ] ## removes duplicates
 
 # download GADM-L3 shapefile (courtesy: http://www.gadm.org)
 download("https://www.dropbox.com/s/otspr9b9jtuyneh/KEN_adm3.zip?raw=1", "KEN_adm3.zip", mode = "wb")
@@ -42,7 +42,7 @@ projection(yield) <- projection(shape)
 gadm <- yield %over% shape
 yield <- as.data.frame(yield)
 yield <- cbind(gadm[ ,c(5,7,9)], yield)
-colnames(yield) <- c("district","division","location","lon","lat","gpsalt","gpsacc","yield","pdens","flood","striga","slope","sdepth","pltsize","dap","can")
+colnames(yield) <- c("district","division","location","id","lat","lon","year","trt","can","dap","fsize","yield")
 
 # project survey coords to grid CRS
 yield.proj <- as.data.frame(project(cbind(yield$lon, yield$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
@@ -54,12 +54,11 @@ projection(yield) <- projection(yield)
 # extract gridded variables at survey locations
 yieldgrid <- extract(grids, yield)
 gsdat <- as.data.frame(cbind(yield, yieldgrid)) 
-gsdat <- gsdat[!duplicated(gsdat), ] ## removes any duplicates
-gsdat <- gsdat[complete.cases(gsdat[,c(8:9,15:48)]),] ## removes incomplete cases
+gsdat <- gsdat[complete.cases(gsdat[,c(1:3,13:44)]),] ## removes incomplete cases
 
 # Classify yield propensities by conditional quantile ---------------------
 # this is the conditional yield gap based on the current data at median values
-qy.rq <- rq(log(yield)~log(pdens)+dap*can, tau = 0.5, data = gsdat) ## try quantiles other than the median
+qy.rq <- rq(log(yield)~factor(year)+trt+dap*can, tau = 0.5, data = gsdat) ## try quantiles other than the median
 summary(qy.rq)
 gsdat$qy <- as.factor(ifelse(exp(predict(qy.rq, gsdat)) > gsdat$yield, "B", "A"))
 prop.table(table(gsdat$qy))
@@ -75,4 +74,3 @@ w <- leaflet() %>%
   addCircleMarkers(gsdat$lon, gsdat$lat, clusterOptions = markerClusterOptions())
 w ## plot widget 
 saveWidget(w, 'OAF_yield_survey.html', selfcontained = T) ## save widget
-
