9542954169a7102ab931d8f91caedb3784b3c080
mgwalsh <markusgwalsh@gmail.com>
2020-02-03 13:31:43 +0300

    TAMASA maize fertilizer nutrient ommission trials, Central Ethiopia, 2015/16

---
+++ TAMASA_ET_not_data.R
@@ -0,0 +1,57 @@
+# TAMASA maize fertilizer nutrient ommission trials, Central Ethiopia, 2015/16
+# Yield trial data courtesy of CIMMYT
+# M. Walsh, J. Chamberlin, January 2019
+
+# Required packages
+# install.packages(c("downloader","rgdal","raster","arm","quantreg")), dependencies=TRUE)
+suppressPackageStartupMessages({
+  require(downloader)
+  require(rgdal)
+  require(raster)
+  require(arm)
+  require(quantreg)
+})
+
+# Create a data folder in your current working directory
+dir.create("TAMASA_NOT_data", showWarnings=F)
+setwd("./TAMASA_NOT_data")
+dir.create("Results", showWarnings = F)
+rm(list = ls()) ## scrub extraneous objects in memory
+
+# Data downloads -----------------------------------------------------------
+# download TAMASA yield data
+download("https://www.dropbox.com/s/0cefqlb0g584mfd/ET_no_trials.zip?raw=1", "ET_no_trials.zip", mode = "wb")
+unzip("ET_no_trials.zip", overwrite = T)
+sites <- read.table("sites.csv", header=T, sep=",")
+trial <- read.table("trials.csv", header=T, sep=",")
+tresp <- merge(sites, trial, by="tid")
+
+# download GADM-L3 shapefile (courtesy: http://www.gadm.org)
+download("https://www.dropbox.com/s/25kw13359f8l5tr/ET_adm_shp.zip?raw=1", "ET_adm_shp.zip", mode = "wb")
+unzip("ET_adm_shp.zip", overwrite = T)
+shape <- shapefile("ETH_adm3.shp")
+
+# Data setup --------------------------------------------------------------
+# attach GADM-L3 admin unit names from shape
+coordinates(tresp) <- ~lon+lat
+projection(tresp) <- projection(shape)
+gadm <- tresp %over% shape
+tresp <- as.data.frame(tresp)
+tresp <- cbind(gadm[ ,c(5,7,9)], tresp)
+colnames(tresp) <- c("region","zone","woreda","tid","sid","lon","lat","cyld","year","loc","trt","tyld")
+tresp$resp <- tresp$tyld-tresp$cyld
+rlevel <- 2 ## set intended response-level relative to current yield
+tresp$trti <- ifelse(tresp$tyld > rlevel*tresp$cyld, 1, 0)
+
+# Regressions -------------------------------------------------------------
+# Quantile regression, control vs NOT treatment yields
+par(pty="s")
+plot(tyld~cyld, xlab="Control yield (kg/ha)", ylab="Treatment yield (kg/ha)", xlim=c(-5,10005), cex.lab=1.3, tresp)
+YQ <- rq(log(tyld)~log(cyld), tau=c(0.25,0.5,0.75), data=tresp)
+print(YQ)
+curve(exp(YQ$coefficients[1])*x^YQ$coefficients[2], add=T, from=0, to=10000, col="blue", lwd=2)
+curve(exp(YQ$coefficients[3])*x^YQ$coefficients[4], add=T, from=0, to=10000, col="red", lwd=2)
+curve(exp(YQ$coefficients[5])*x^YQ$coefficients[6], add=T, from=0, to=10000, col="blue", lwd=2)
+
+
+
